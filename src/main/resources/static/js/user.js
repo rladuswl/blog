42f2a1bedda790f3985643f89120fa843c613ae3
rladuswl <khkkgon18@naver.com>
2022-02-18 18:07:05 +0900

    Ajax 요청

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,44 @@
+let index = {
+    init: function () {
+        $("#btn-save").on("click", ()=>{ // function(){} 대신 ()=>{} 를 쓴 이유 : this를 바인딩하기 위해서
+            this.save();
+        });
+    },
+
+    save: function () {
+        // alert('user의 save함수 호출됨');
+        let data = {
+            username: $("#username").val(),
+            password: $("#password").val(),
+            email: $("#email").val()
+        };
+
+        // console.log(data); data는 자바스크립트 오브젝트
+        // console.log(JSON.stringify(data)); JSON 문자열
+
+        // ajax 호출시 default가 비동기 호출 -> 순서 상관없음
+        // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
+        // ajax가 통신을 성공하고 서버가 json을 리턴해주면 자동으로 자바(스크립트?) 오브젝트로 변환
+        $.ajax({
+            // 회원가입 수행 요청
+            type: "POST",
+            url: "/blog/api/user",
+            data: JSON.stringify(data), // http body 데이터
+            contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지 (MIME)
+            dataType: "json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 모든 것이 String(문자열), 만약 생긴게 json이라면 javascript 오브젝트로 변경
+        }).done(function (resp) {
+            // 결과가 정상이면 done 실행
+            alert("회원가입이 완료되었습니다.");
+            //console.log(resp);
+            location.href = "/blog";
+        }).fail(function (error) {
+            // 실패하면 fail 실행
+            alert("회원가입이 실패하였습니다.");
+            alert(JSON.stringify(error));
+        });
+    }
+}
+
+
+index.init();
+
--- src/main/webapp/WEB-INF/views/layout/header.jsp
+++ src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -2,10 +2,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Bootstrap Example</title>
+    <title>YJ 블로그</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 
