36016ba282d4ee3ad7d386583f8b16e7617302f2
rladuswl <khkkgon18@naver.com>
2022-02-22 02:09:07 +0900

    글목록 페이징처리

--- src/main/java/com/yj/blog/controller/BoardController.java
+++ src/main/java/com/yj/blog/controller/BoardController.java
@@ -1,12 +1,19 @@
 package com.yj.blog.controller;
 
 import com.yj.blog.config.auth.PrincipalDetail;
+import com.yj.blog.model.Board;
 import com.yj.blog.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class BoardController {
@@ -14,9 +21,14 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    // 페이징 url : http://localhost:8000/?page=0
     @GetMapping({"", "/"})
-    public String index(Model model) {
-        model.addAttribute("boards", boardService.글목록());
+    public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+//        Page<Board> pagingBoard = boardService.글목록(pageable);
+//        List<Board> boards = pagingBoard.getContent(); // getContent() 하면 List 타입으로 받을 수 있음
+//        model.addAttribute("boards", boards);
+
+        model.addAttribute("boards", boardService.글목록(pageable)); // List 가 아닌 Page 타입으로..
 
         // /WEB-INF/views/index.jsp
         return "index"; // viewResolver 작동 (@Controller)
