e7ae3cd511ffbe0b5c4621f25a3e7fe2f43a3cf4
rladuswl <khkkgon18@naver.com>
2022-02-21 23:07:18 +0900

    글 작성 기능 - jsp, js

---
+++ src/main/resources/static/js/board.js
@@ -0,0 +1,34 @@
+let index = {
+    init: function () {
+        $("#btn-save").on("click", ()=>{
+            this.save();
+        });
+    },
+
+    save: function () {
+        let data = {
+            title: $("#title").val(),
+            content: $("#content").val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/api/board",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function (resp) {
+            // 결과가 정상이면 done 실행
+            alert("글작성이 완료되었습니다.");
+            location.href = "/";
+        }).fail(function (error) {
+            // 실패하면 fail 실행
+            alert("회원가입이 실패하였습니다.");
+            alert(JSON.stringify(error));
+        });
+    },
+}
+
+
+index.init();
+
