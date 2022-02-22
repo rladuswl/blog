547e7ea968ed6d190c66166f27f076169bd8fce1
rladuswl <khkkgon18@naver.com>
2022-02-22 14:03:54 +0900

    글 상세보기 기능 구현완료

--- src/main/java/com/yj/blog/controller/BoardController.java
+++ src/main/java/com/yj/blog/controller/BoardController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -21,7 +22,7 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
-    // 페이징 url : http://localhost:8000/?page=0
+    // 메인페이지 페이징 url : http://localhost:8000/?page=0
     @GetMapping({"", "/"})
     public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
 //        Page<Board> pagingBoard = boardService.글목록(pageable);
@@ -34,8 +35,16 @@ public class BoardController {
         return "index"; // viewResolver 작동 (@Controller)
     }
 
+    // 글 작성 폼
     @GetMapping("/board/saveForm")
     public String saveForm() {
         return "board/saveForm";
+    }
+
+    // 글 상세보기
+    @GetMapping("/board/{id}")
+    public String findById(@PathVariable int id, Model model) {
+        model.addAttribute("board", boardService.글상세보기(id));
+        return "board/detail";
     }
 }
