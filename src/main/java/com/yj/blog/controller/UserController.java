3c9d02dcd541881a5065ab7b6bc1525c73929f5b
rladuswl <khkkgon18@naver.com>
2022-02-24 03:04:09 +0900

    카카오 로그인 요청 및 Access Token 받기

--- src/main/java/com/yj/blog/controller/UserController.java
+++ src/main/java/com/yj/blog/controller/UserController.java
@@ -1,9 +1,17 @@
 package com.yj.blog.controller;
 
 import com.yj.blog.config.auth.PrincipalDetail;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 // 인증이 안된 사용자들이 출입할 수 있는 경로 : /auth/** (auth의 하위) 허용, 주소가 그냥 / 이면 index.jsp로 가는데 이 곳도 허용, static 이하에 있는 /js/**, /css/**, /image/** 허용
 
@@ -23,8 +31,42 @@ public class UserController {
     }
 
     @GetMapping("/user/updateForm")
-    public String updateForm(@AuthenticationPrincipal PrincipalDetail principal) {
+    public String updateForm() {
 
         return "user/updateForm";
+    }
+
+    @GetMapping("/auth/kakao/callback")
+    public @ResponseBody String kakaoCallback(String code) { // @ResponseBody : Data를 리턴해주는 컨트롤러 함수
+
+        // 카카오 API 서버에게 POST 방식으로 key=value 데이터를 요청
+        // 요청 방법 -> 여러가지 라이브러리  : HttpsURLConnection, Retrofit2(주로 안드로이드), OkHttp, RestTemplate
+        RestTemplate rt = new RestTemplate();
+
+        // HttpHeader 객체 생성
+       HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8"); // key=value 형태의 데이터라는 것을 알려주는 부분
+
+        // HttpBody 객체 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "d5526e3b2a4169a3d9f2b7a6f9a12cdf");
+        params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback");
+        params.add("code", code);
+
+        // HttpHeader와 HttpBody를 하나의 객체에 담기 -> 만든 이유 : 아래의 exchange 함수에 HttpEntity를 넣어야 해서..
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+                new HttpEntity<>(params, headers); // body 데이터와 headers 값을 가지고 있는 Entity
+
+        // 카카오에게 Http 요청하기 (POST 방식) -> response라는 변수에 응답을 받음
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+//        return "카카오 인증 완료 : 코드값 : " + code;
+        return "카카오 토큰 요청 완료 : 토큰요청에 대한 응답 : " + response;
     }
 }
--- src/main/webapp/WEB-INF/views/user/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/user/loginForm.jsp
@@ -17,6 +17,7 @@
         </div>
 
         <button id="btn-login" class="btn btn-primary">로그인</button>
+        <a href="https://kauth.kakao.com/oauth/authorize?client_id=d5526e3b2a4169a3d9f2b7a6f9a12cdf&redirect_uri=http://localhost:8000/auth/kakao/callback&response_type=code"><img height="38px" src="/image/kakao_login_button.png" /></a>
 
     </form>
 </div>
